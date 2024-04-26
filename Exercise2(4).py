ae7e0b1622a3e3c205974e12a2547eb5184db03d
filifil <filifilibro@gmail.com>
2024-04-26 02:43:56 +0200

    Exercise 2

---
+++ Exercise2(4).py
@@ -0,0 +1,10 @@
+num = int(input('Enter a number:'))
+
+if num > 1000 and num < 2000:
+    print('This number is between 1000 and 2000:',num)
+elif num < 1000:
+    print('less number:',num)
+elif num > 2000:
+    print('more number:',num)
+else:
+    print('excluded:',num)
