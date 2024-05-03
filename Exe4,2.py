4e84d830de9e5513161861288367cb7de14eaa49
filifil <filifilibro@gmail.com>
2024-05-03 03:51:45 +0200

    wel doing

---
+++ Exe4,2.py
@@ -0,0 +1,7 @@
+permit1 = [10,20,8,4]
+permit2 = [16,22,6,14]
+permit = permit1 + permit2
+
+print("The full list of Frank's sickdays is",permit)
+print("He was sick the most in quarter",permit1.index(max(permit1)),'and',permit2.index(max(permit2)),"of the year.")
+print('so He sicks often quarter',1)
